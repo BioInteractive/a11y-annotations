9a449242e4886e30ae97f1163c401d8b22dcf3e7
BioInteractive <74253534+BioInteractive@users.noreply.github.com>
2024-05-01 04:14:20 +0000

    Huge Reorganization and Auditor Checklist Completion

--- assets/js/global.js
+++ assets/js/global.js
@@ -26,3 +26,63 @@ document.querySelectorAll(".copy-btn").forEach((button) => {
     });
 });
 
+// Checklist Script to save and reset local storage
+// Document ready function
+$(document).ready(function() {
+// Hide the alert message initially
+$('#confirmAlert').hide();
+
+// Function to load the checklist state
+function loadChecklistState() {
+    // Loop through all the checkboxes in the checklist
+    $('input[type="checkbox"]').each(function() {
+    var id = $(this).attr('id');
+    var checked = localStorage.getItem(id) === 'true';
+    $(this).prop('checked', checked);
+    });
+}
+
+// Function to save the checklist state
+function saveChecklistState() {
+    // Loop through all the checkboxes in the checklist
+    $('input[type="checkbox"]').each(function() {
+    var id = $(this).attr('id');
+    var checked = $(this).is(':checked');
+    localStorage.setItem(id, checked);
+    });
+}
+
+// Event listener for checkbox state change
+$('input[type="checkbox"]').change(function() {
+    saveChecklistState();
+});
+
+// Load checklist state from local storage on page load
+loadChecklistState();
+
+// Event listener for the reset button
+$('#clearBtn').click(function() {
+    $('#confirmAlert').show(); // Show the alert
+});
+
+// Event listener for the confirm button in the alert
+$('#confirmBtn').click(function() {
+    // Loop through all the checkboxes and clear them
+    $('input[type="checkbox"]').each(function() {
+    $(this).prop('checked', false);
+    });
+    
+    // Clear the local storage
+    localStorage.clear();
+
+    // Hide the alert again
+    $('#confirmAlert').hide();
+
+    // Set focus back to the reset button
+    $('#clearBtn').focus();
+
+    // You may want to reload the page to reset all form controls
+    // location.reload();
+    });
+});
+
